5a0988541a5c0482a312fcac119950d9e91e2811
gouthamraj020102 <gouthamraj020102@gmail.com>
2023-03-22 19:52:24 +0530

    Setting up Bootstrap

---
+++ 32. Bootstrap Templates/src/main/java/com/project/smartContactManager/Controllers/HomeController.java
@@ -0,0 +1,21 @@
+package com.project.smartContactManager.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/home")
+	public String home(Model model) {
+		model.addAttribute("title", "Home - Smart Contact Manager");
+		return "home";
+	}
+
+	@GetMapping("/about")
+	public String about(Model model) {
+		model.addAttribute("title", "About - Smart Contact Manager");
+		return "about";
+	}
+}
