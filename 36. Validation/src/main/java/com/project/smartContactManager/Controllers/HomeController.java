3b64df16d216e7a528df2f933574f90613d18fb0
gouthamraj020102 <gouthamraj020102@gmail.com>
2023-04-06 10:00:55 +0530

    Server side Validation

---
+++ 36. Validation/src/main/java/com/project/smartContactManager/Controllers/HomeController.java
@@ -0,0 +1,79 @@
+package com.project.smartContactManager.Controllers;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.project.smartContactManager.Entities.User;
+import com.project.smartContactManager.Helper.Message;
+import com.project.smartContactManager.Repositories.UserRepository;
+
+@Controller
+public class HomeController {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@GetMapping("/")
+	public String home(Model model) {
+		model.addAttribute("title", "Home - Smart Contact Manager");
+		return "home";
+	}
+
+	@GetMapping("/about")
+	public String about(Model model) {
+		model.addAttribute("title", "About - Smart Contact Manager");
+		return "about";
+	}
+
+	@GetMapping("/signup")
+	public String signUp(Model model) {
+		model.addAttribute("title", "Register - Smart Contact Manager");
+		model.addAttribute("user", new User());
+		return "signup";
+	}
+
+	@PostMapping("/do_register")
+	public String registerUser(@Valid @ModelAttribute("user") User user, BindingResult res,
+			@RequestParam(value = "agreement", defaultValue = "false") boolean agreement, Model model,
+			HttpSession session) {
+
+		try {
+			if (!agreement) {
+				System.out.println("You have not agreed to Terms & Conditions");
+				throw new Exception("You have not agreed to Terms & Conditions");
+			}
+
+			if (res.hasErrors()) {
+				System.out.println("ERROR" + res.toString());
+				model.addAttribute("user", user);
+				return "signup";
+			}
+			user.setRole("ROLE_USER");
+			user.setEnabled(true);
+			user.setImage("default.png");
+
+			System.out.println("Agreement:" + agreement);
+			System.out.println("User:" + user);
+
+			this.userRepository.save(user);
+			model.addAttribute("user", new User());
+
+			session.setAttribute("message", new Message("Successfully registered !!", "alert-success"));
+			return "signup";
+		} catch (Exception e) {
+			e.printStackTrace();
+			model.addAttribute("user", user);
+			session.setAttribute("message", new Message("Something went wrong !!" + e.getMessage(), "alert-danger"));
+			return "signup";
+		}
+	}
+}
